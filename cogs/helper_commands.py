23d72450dfc4245c1bf7d8171715f039f60246ea
Kacper <kacperinos@gmail.com>
2022-07-13 16:07:24 +0100

    Add mute and unmute live commands

--- cogs/helper_commands.py
+++ cogs/helper_commands.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
-
+from discord.utils import get
+import discord
 
 class HelperCommands(commands.Cog):
     def __init__(self, client):
@@ -10,6 +11,31 @@ class HelperCommands(commands.Cog):
     async def echo(self, ctx, *, message):
         await ctx.message.delete()
         await ctx.send(message)
+
+    @commands.command(name='mutelive')
+    @commands.has_guild_permissions(administrator=True)
+    async def mutelive(self, ctx, user: discord.Member=None):
+        if not user:
+            return
+        mutelive_role = get(ctx.guild.roles, name='⚠︎')
+
+        if mutelive_role in user.roles:
+            return
+
+        await user.add_roles(mutelive_role)
+
+    @commands.command(name='unmutelive')
+    @commands.has_guild_permissions(administrator=True)
+    async def unmutelive(self, ctx, user: discord.Member = None):
+        if not user:
+            return
+        mutelive_role = get(ctx.guild.roles, name='⚠︎')
+
+        if mutelive_role not in user.roles:
+            return
+
+        await user.remove_roles(mutelive_role)
+
 
 def setup(client):
     client.add_cog(HelperCommands(client))
